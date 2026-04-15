c2fe62afd3d721e7195512a545248c0e49afa4ab
z007samok <z007samok@gmail.com>
2026-04-15 13:28:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function simpanKey() {
 
 function aktifkanApp() {
     document.getElementById('setup-api').style.display = 'none';
-    GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+    GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + API_KEY;
     
     // TUKAR BARIS DI BAWAH INI:
     binaMenuUtama(); 
